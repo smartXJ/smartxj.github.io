82e4a82cfca1477ce13310f18fc6a60c02767bf8
xiaojun <xj517356221@163.com>
2021-04-16 22:17:03 +0800

    Site updated: 2021-04-16 22:17:02

--- js/comment.js
+++ js/comment.js
@@ -9,5 +9,5 @@ console.log('%c 欢迎来到我的博客！',
 
 console.log('%c 既然来啦，就留个言吧',
     'background-color: #49b1f5; color: #fff; padding: 8px;font-size:14px;',
-    'https://shuxhan.com/message')
+    'https://smartxj.github.io/message')
 
